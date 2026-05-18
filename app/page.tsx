d625e95cbec69ab00367921a0b92ec37a150f87f
ManfredsGitHub <135500963+ManfredsGitHub@users.noreply.github.com>
2026-05-18 18:01:09 +0200

    Shorten Landtag RLP card title to prevent line break

--- app/page.tsx
+++ app/page.tsx
@@ -88,8 +88,8 @@ const ABGEORDNETE = [
     awUrl: 'https://www.abgeordnetenwatch.de/profile/isabel-mackensen-geis',
   },
   {
-    name: 'Landtag RLP – Abgeordnetensuche', partei: 'RLP', ebene: 'Landtag RLP',
-    wahlkreis: 'Rhein-Pfalz-Kreis · alle Fraktionen',
+    name: 'Landtag RLP', partei: 'RLP', ebene: 'Landtag RLP',
+    wahlkreis: 'Rhein-Pfalz-Kreis · alle Abgeordneten',
     parteifarbe: 'bg-purple-600', farbe: 'bg-purple-50 border-purple-200', textfarbe: 'text-purple-900',
     profilUrl: 'https://www.landtag.rlp.de/de/parlament/abgeordnete/abgeordnetensuche/',
     awUrl: 'https://www.abgeordnetenwatch.de/rheinland-pfalz/abgeordnete',
